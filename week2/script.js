e0b73a33b6598bb9e261f2952c1320507295b8b9
Kemirdin <qemirdin@gmail.com>
2018-01-24 18:01:14 +0100

    update file

---
+++ week2/script.js
@@ -0,0 +1,14 @@
+/*Say you would like to write a program that doubles the odd numbers in an array and throws away the even number.
+
+Your solution could be something like this:*/
+
+let numbers = [1, 2, 3, 4];
+let newNumbers = [];
+
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 2 !== 0) {
+        newNumbers.push(numbers[i] * 2);
+    }
+}
+
+console.log("The doubled numbers are", newNumbers); // [2, 6]
