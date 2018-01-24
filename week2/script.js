8f0daf389301dd0af80b78c5aff73928482c4671
Kemirdin <qemirdin@gmail.com>
2018-01-24 18:02:25 +0100

    update file

--- week2/script.js
+++ week2/script.js
@@ -12,3 +12,17 @@ for(let i = 0; i < numbers.length; i++) {
 }
 
 console.log("The doubled numbers are", newNumbers); // [2, 6]
+
+//map & filter. Transform values
+
+var numbers = [1, 2, 3, 4];
+
+var newNumbers = numbers
+  .filter (function (number) {
+    return number % 2 !== 0;
+  })
+  .map (function (number) {
+    return number * 2;
+  });
+
+console.log ('The doubled numbers are', newNumbers); // [2, 6]
