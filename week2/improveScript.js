846e230fcc58b1a3803f3793a2d00e1f845f050e
Kemirdin <qemirdin@gmail.com>
2018-01-25 20:31:58 +0100

    update file

--- week2/improveScript.js
+++ week2/improveScript.js
@@ -57,63 +57,14 @@ const amounts = shortDurations.map(duration =>  {
 
 
 let totalInEuros = 0;
-amounts.forEach(amount => ...);
-console.log(...);
+amounts.forEach(amount =>{
+    return '€' + amount.toFixed (2);
+  });
+console.log(totalInEuros);
 
-products.forEach (product => {
+/*products.forEach (product => {
   product.sizes.forEach (size => {
     console.log (size);
   });
-});
-
-
- let totalInEuros= amounts.map (function (dollar_amount) {
-    return '$' + dollar_amount.toFixed (2);
-  })
-  .reduce (function (formatted_dollar_amount) {
-    return formatted_dollar_amount;
-  });
-
-
-
-
-// Convert the task durations to hours, instead of minutes.
-
-let getHour = tasks.filter(function (number) {
-    return number>0  !== 0;
-}).map((number) => {
-   
-    /* let hours = Math.floor (duration / 60);*/
-  
-    return hours = Math.trunc(number / 60) // I used Math.trunc(), which is a new method added in 2015.
-    minutes = number % 60;
-})
-  console.log (hours + ':' + minutes);
-
-// or  Convert the task durations to hours, instead of minutes.
-
-let getHour = tasks.map((task) => {
-    return (task.duration / 60);
-})
-console.log(getHour);
-
-//Filter out everything that took two hours or more (remove from the collection)
-
-let tookHour = tasks.filter((duration) => {
-    return duration >= 2;
-})
-console.log(tookHour);
-
-//Multiply the each duration by a per-hour rate for billing (you can decide yourself what Maartje should make per hour) and sum it all up.
-
-let currentValue= tasks.map((duration) =>{
-                    return duration * 50;
-}).reduce( (accumulator, currentValue)=> {
-                    return [(+accumulator) + (+current)+'€';
-];
-})
-    console.log(currentValue + '€');
-
-
-
+});*/
 
