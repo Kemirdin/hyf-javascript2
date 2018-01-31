3f417b9e7261200ecbf17b347eeae4b7e203058f
Kemirdin <qemirdin@gmail.com>
2018-01-31 23:04:27 +0100

    update file

--- week3/homeWork.js
+++ week3/homeWork.js
@@ -1,1 +1,6 @@
 'use strict'
+
+//1.Your task is to write a function that takes another function as an argument and runs it.
+console.log (
+  '\n***************question 1 ************************************************************\n'
+);
