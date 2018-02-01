79bf455e3f798d9e912d7d796a70beadfd812fb7
Kemirdin <qemirdin@gmail.com>
2018-02-01 19:29:37 +0100

    update file

--- week3/homeWork.js
+++ week3/homeWork.js
@@ -48,5 +48,54 @@ console.log(
 //Please read something about setTimeout on MDN.The first argument to the setTimeout call is the callback(doIt)
  
 setTimeout(function () {
-  console.log("I will be executed in 2 seconds.");
+  console.log("I will be executed in 5 seconds.");
 }, 5000);
+
+// You must write a
+// function that takes 4 arguments. - A start value - An end value - A callback to call
+// if the number is divisible by 3 - A callback to use
+// if the number is divisible by 5
+
+// The
+// function should generate an array containing values from start value to end value(inclusive).
+
+// Then the
+// function should iterate over the array and call the first callback
+// if the array value is divisible by 3
+
+// The
+// function should call the second callback
+// if the array value is divisible by 5
+
+// Both functions should be called
+// if the array value is divisible by both 3 and 5
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(
+  '\n***************question 3 ************************************************************\n'
+);
+
+
+function repeatStringNumTimes(str, num) {
+
+  var newStr = '';
+
+  if (num < 0) return newStr;
+
+  newStr = str.repeat(num);
+
+  return newStr;
+
+}
+
+repeatStringNumTimes("abc", 3);
