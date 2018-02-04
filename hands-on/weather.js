3c0f1ba05e7c2eb0e325d8f723bab6168486e38f
Kemirdin <qemirdin@gmail.com>
2018-02-04 15:03:44 +0100

    update file

--- hands-on/weather.js
+++ hands-on/weather.js
@@ -16,3 +16,14 @@ function toFahrenheut (degreesCelsius) {
 }
 console.log(`the temperature in new york is ${toCelcius(44).toFixed(2)} c`);
 console.log(`the temperature in Amsterdam is ${toFahrenheut(3).toFixed(2)} F`);
+
+
+let toCelcius = (degreesFahrenheut)=>  (degreesFahrenheut - 32) * (5 / 9);
+let toFahrenheut = (degreesCelsius) => (degreesCelsius * (9 / 5) + 32);
+function weatherReport(city, digress, unit, reporterFn) {
+  if (unit==='C')
+    return `the temperature in ${city} is ${reporterFn(digress).toFixed(2)} F`
+  else
+  return   `the temperature in ${city} is ${reporterFn (digress).toFixed (2)} C`;
+
+}
